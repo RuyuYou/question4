f92521a2b87b7c2194f76bfbb68de2a16457f412
Ruyu You <727809166@qq.com>
2016-05-23 22:22:00 +0800

    finish collect_all_even.js

--- src/collect_all_even.js
+++ src/collect_all_even.js
@@ -1,6 +1,21 @@
-
 function collect_same_elements(collection_a, object_b) {
- //在此处写代码
+    var sameElements = buildSameElements(collection_a, object_b);
+    return sameElements;
 }
+
+function buildSameElements(collection_a, object_b) {
+    var sameElements = [];
+    collection_a.forEach(function (array) {
+        for (var j = 0; j < object_b.value.length; j++) {
+            if (array.key === object_b.value[j]) {
+                sameElements.push(array.key);
+            }
+        }
+    });
+    return sameElements;
+}
+
+module.exports = collect_same_elements;
+
 
 module.exports = collect_same_elements;
